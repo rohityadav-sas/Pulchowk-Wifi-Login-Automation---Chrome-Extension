2bc9f101861be6a9f5b4ea49f224a721737aa6cf
Rohit Yadav <rohityadav.se@gmail.com>
2023-12-21 21:34:26 +0545

    Bug Fix

--- background.js
+++ background.js
@@ -6,9 +6,7 @@ chrome.tabs.onUpdated.addListener((tabId, changeInfo, tab) => {
 
 chrome.runtime.onMessage.addListener((data, sender, sendResponse) => {
   if (data.closeTab === "yes") {
-    chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
-      chrome.tabs.remove(tabs[0].id);
-    });
+    chrome.tabs.remove(sender.tab.id);
   }
   if (data.redirect === "yes") {
     chrome.tabs.create({ url: "https://10.100.1.1:8090/" });
